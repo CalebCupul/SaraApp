4bee4d74067ace2c19f1fbb7db84e904a98e494a
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-04-08 19:48:24 -0600

    feat: add custom font

--- App.js
+++ App.js
@@ -1,36 +1,70 @@
+import { Urbanist_700Bold as UrbanistBold } from "@expo-google-fonts/urbanist";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
 import { StyleSheet } from "react-native";
-import { AcademicCapIcon, CalendarDaysIcon, UserCircleIcon } from "react-native-heroicons/outline";
+import {
+  AcademicCapIcon,
+  CalendarDaysIcon,
+  UserCircleIcon,
+} from "react-native-heroicons/outline";
 import EventsScreen from "./screens/events/EventsScreen";
 import RecordsScreen from "./screens/records/RecordsScreen";
 import ProfileScreen from "./screens/users/ProfileScreen";
 
-
+SplashScreen.preventAutoHideAsync()
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    UrbanistBold,
+  });
+
+  const onLayourRootView = useCallback( async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="dark"></StatusBar>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Events" component={EventsScreen} options={{
-            tabBarIcon: () => {
-              return <CalendarDaysIcon color="gray"/>
-            }
-          }}></Tab.Screen>
-          <Tab.Screen name="Records" component={RecordsScreen} options={{
-            tabBarIcon: () => {
-              return <AcademicCapIcon color="gray"/>
-            }
-          }}></Tab.Screen>
-          <Tab.Screen name="Profile" component={ProfileScreen} options={{
-            tabBarIcon: () => {
-              return <UserCircleIcon color="gray"/>
-            }
-          }}></Tab.Screen>
+          <Tab.Screen
+            name="Events"
+            component={EventsScreen}
+            options={{
+              tabBarIcon: () => {
+                return <CalendarDaysIcon color="gray" />;
+              },
+            }}
+          ></Tab.Screen>
+          <Tab.Screen
+            name="Records"
+            component={RecordsScreen}
+            options={{
+              tabBarIcon: () => {
+                return <AcademicCapIcon color="gray" />;
+              },
+            }}
+          ></Tab.Screen>
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: () => {
+                return <UserCircleIcon color="gray" />;
+              },
+            }}
+          ></Tab.Screen>
         </Tab.Navigator>
       </NavigationContainer>
     </>
--- screens/records/RecordsScreen.js
+++ screens/records/RecordsScreen.js
@@ -1,9 +1,11 @@
-import { Text } from "react-native-svg";
+import { Text } from "react-native";
 
-function RecordsScreen(){
-    return (
-        <Text>Records</Text>
-    )
+function RecordsScreen() {
+  return (
+    <Text style={{ fontFamily: "UrbanistBold" }} className="m-4 text-lg">
+      Resume
+    </Text>
+  );
 }
 
-export default RecordsScreen
+export default RecordsScreen;
