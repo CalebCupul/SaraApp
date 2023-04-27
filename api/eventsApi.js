fec6887e2667c505782a47900ebe2789a208541e
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-04-27 11:15:27 -0600

    feat: add .env

--- api/eventsApi.js
+++ api/eventsApi.js
@@ -1,5 +1,5 @@
+import { API_BASE_URL } from "@env";
 import axios from "axios";
-import { API_BASE_URL } from "../config/config";
 
 export const getEvents = async (page = 1) => {
     try {
--- config/config.js
+++
@@ -1,3 +0,0 @@
-// export const API_BASE_URL = 'http://192.168.100.77:8000/api/v1';
-export const API_BASE_URL = 'https://93b3-201-116-196-134.ngrok.io/api/v1';
-
