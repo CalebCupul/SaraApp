5005797c642d7fc0e3b093e4318374cf71450419
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-05-22 21:12:48 -0600

    fix: qr scanner admin only

--- screens/events/EventDetailsScreen.js
+++ screens/events/EventDetailsScreen.js
@@ -3,7 +3,7 @@ import { Image, Pressable, ScrollView, Text, View } from "react-native";
 import {
   ClockIcon,
   MapPinIcon,
-  QrCodeIcon
+  QrCodeIcon,
 } from "react-native-heroicons/outline";
 import QrCodeScanner from "../../components/QrCodeScanner";
 import { UserContext } from "../../contexts/UserContext";
@@ -11,14 +11,14 @@ import { UserContext } from "../../contexts/UserContext";
 function EventDetailsScreen({ route }) {
   const event = route.params.props.event;
   const userCtx = useContext(UserContext);
-  const [showScanner, setShowScanner] = useState(false)
+  const [showScanner, setShowScanner] = useState(false);
   // const description =
-//     "¡Bienvenidos al evento de La relación de la contaminación con los cocodrilos de Puerto Vallarta! Este será un encuentro especial para abordar la problemática que representa la contaminación en la región y su impacto en la fauna local, en particular en los cocodrilos. La cita será en el hermoso puerto de Vallarta, ubicado en la costa del Pacífico mexicano, el cual es reconocido por su belleza natural, su cultura y su gastronomía. Durante este evento, expertos en medio ambiente y conservación, así como miembros de la comunidad local, compartirán información y experiencias sobre la relación entre la contaminación y los cocodrilos de la región. La presencia de estos animales es fundamental para el equilibrio ecológico de la zona, sin embargo, los niveles de contaminación y la falta de conciencia ambiental están poniendo en riesgo su supervivencia.";
-    if ( showScanner ){
-      return <QrCodeScanner eventId={event.id} token={userCtx.token}/>
-    }
+  //     "¡Bienvenidos al evento de La relación de la contaminación con los cocodrilos de Puerto Vallarta! Este será un encuentro especial para abordar la problemática que representa la contaminación en la región y su impacto en la fauna local, en particular en los cocodrilos. La cita será en el hermoso puerto de Vallarta, ubicado en la costa del Pacífico mexicano, el cual es reconocido por su belleza natural, su cultura y su gastronomía. Durante este evento, expertos en medio ambiente y conservación, así como miembros de la comunidad local, compartirán información y experiencias sobre la relación entre la contaminación y los cocodrilos de la región. La presencia de estos animales es fundamental para el equilibrio ecológico de la zona, sin embargo, los niveles de contaminación y la falta de conciencia ambiental están poniendo en riesgo su supervivencia.";
+  if (showScanner) {
+    return <QrCodeScanner eventId={event.id} token={userCtx.token} />;
+  }
 
-    return (
+  return (
     <ScrollView className="bg-white">
       <View className="p-4 pt-14">
         <View className="flex-row justify-between">
@@ -28,7 +28,7 @@ function EventDetailsScreen({ route }) {
             </Text>
             <Text style={{ fontFamily: "UrbanistBold" }}>{event.ponente}</Text>
 
-            <View className="mt-4 flex flex-row items-center space-x-1">
+            <View className="mt-2 flex flex-row items-center space-x-1">
               <ClockIcon color={"#1C1C1E"} width={18} />
               <Text
                 style={{ fontFamily: "UrbanistMedium" }}
@@ -38,18 +38,19 @@ function EventDetailsScreen({ route }) {
               </Text>
             </View>
 
-            <View className="flex flex-row items-center space-x-1">
-              <MapPinIcon color={"#1C1C1E"} width={18} />
+            <View className="flex flex-row space-x-1">
+                <MapPinIcon color={"#1C1C1E"} width={18} />
               <Text
                 style={{ fontFamily: "UrbanistMedium" }}
                 className="text-dark-gray"
-              >{event.lugar}
+              >
+                {event.titulo}
               </Text>
             </View>
           </View>
           <Image
             className="w-16 h-16 object-contain rounded-md"
-            source={require('../../assets/events/eventImage.png')}
+            source={require("../../assets/events/eventImage.png")}
           />
         </View>
         {/* <View>
@@ -94,7 +95,7 @@ function EventDetailsScreen({ route }) {
           </View>
         </View> */}
 
-        {userCtx.userInfo.role_id != 1 ? (
+        {userCtx.userInfo.role_id != 0 ? (
           <Pressable onPress={() => setShowScanner(!showScanner)}>
             <View className="mx-auto mt-4 py-1 px-4 flex flex-row space-x-1 rounded-full bg-dark-gray items-center justify-center w-60">
               <QrCodeIcon color={"white"} width={30} />
